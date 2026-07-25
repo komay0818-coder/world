5b6ea398f0e9565eb0aad979d4f1ee062238639a
komay0818-coder <komay0818-coder@users.noreply.github.com>
2026-07-26 03:04:21 +0800

    更換人類戰士戰鬥圖片

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const raceTotems = { human: '☀', elf: '❈', orc: '⛧', undead: '☾' };
 const jobMarks = { warrior: '⛨', assassin: '◈', hunter: '➶', mage: '✦', priest: '✥' };
 const CHARACTER_SCALE = 0.90;
 const battleCharacterArt = {
-  'human:warrior': 'assets/character-sprites/human-warrior.png',
+  'human:warrior': 'assets/character-sprites/human-warrior-v2.png',
   'human:assassin': 'assets/character-sprites/human-assassin-v4.png',
   'human:hunter': 'assets/character-sprites/human-hunter.png',
   'human:mage': 'assets/character-sprites/human-mage.png',
