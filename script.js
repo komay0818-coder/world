2585310f1ca5c137b0d19af949ff390848633e4d
komay0818-coder <komay0818-coder@users.noreply.github.com>
2026-07-31 21:38:43 +0800

    art: update orc warrior character image

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const battleCharacterArt = {
   'elf:hunter': 'assets/character-sprites/elf-hunter.png',
   'elf:mage': 'assets/character-sprites/elf-mage.png',
   'elf:priest': 'assets/character-sprites/elf-priest.png',
-  'orc:warrior': 'assets/character-sprites/orc-warrior-v2.png?v=20260731-user-image-v1',
+  'orc:warrior': 'assets/character-sprites/orc-warrior-v2.png?v=20260731-user-image-v2',
   'orc:assassin': 'assets/character-sprites/orc-assassin.png',
   'orc:hunter': 'assets/character-sprites/orc-hunter.png',
   'orc:mage': 'assets/character-sprites/orc-mage.png',
