e4fac87e32f03796a2883d99abece3212e44603b
komay0818-coder <komay0818-coder@users.noreply.github.com>
2026-08-09 06:03:41 +0800

    feat: add transparent Blackstone lion guard artwork

--- blackstone-stronghold-policy.js
+++ blackstone-stronghold-policy.js
@@ -43,7 +43,7 @@
     monster('blackstone-crossbowman', '黑石弩手', 'normal', 'goblin', '遠程輸出', { image: 'assets/blackstone-crossbowman.png', faction: 'blackstone-goblins' }),
     monster('blackstone-berserker', '黑石狂戰士', 'normal', 'orc', '雙斧近戰', { image: 'assets/blackstone-berserker.png' }),
     monster('blackstone-warhound', '黑石戰犬', 'normal', 'beast', '高速近戰', { image: 'assets/blackstone-warhound.png', faction: 'blackstone-beasts' }),
-    monster('blackstone-lion-guard', '黑石獅衛', 'elite', 'lionkin', '高攻擊近戰'),
+    monster('blackstone-lion-guard', '黑石獅衛', 'elite', 'lionkin', '高攻擊近戰', { image: 'assets/blackstone-lion-guard.png' }),
     monster('blackstone-bullhorn-warrior', '黑石蠻角勇士', 'elite', 'bullkin', '高血量／重擊', { image: 'assets/blackstone-bullhorn-warrior.png' }),
     monster('blackstone-warlord', '黑石督軍', 'boss', 'orc', '據點指揮官', { image: 'assets/blackstone-warlord.png' })
   ]);
