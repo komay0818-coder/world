4fd81af2e54c204461c1b52ca4f482b18cf6e247
komay0818-coder <komay0818-coder@users.noreply.github.com>
2026-08-09 03:30:17 +0800

    feat: add transparent venom spitter artwork

--- spider-nest-policy.js
+++ spider-nest-policy.js
@@ -49,7 +49,7 @@
 
   const MONSTERS = Object.freeze([
     monster('spider-nest-blackstone-poison-spider', '黑石毒蜘蛛', 'normal', '近戰毒系怪物', { attackProfile: 'melee-poison', skillConcepts: Object.freeze(['poison-basic-attacks']) }),
-    monster('venom-spitter-spider', '噴毒蜘蛛', 'normal', '遠程持續傷害怪物', { attackProfile: 'ranged-poison', skillConcepts: Object.freeze(['venom-spit', 'poison-dot']) }),
+    monster('venom-spitter-spider', '噴毒蜘蛛', 'normal', '遠程持續傷害怪物', { image: 'assets/venom-spitter-spider.png', attackProfile: 'ranged-poison', skillConcepts: Object.freeze(['venom-spit', 'poison-dot']) }),
     monster('web-weaver', '蛛網編織者', 'normal', '蛛絲控制型怪物', { attackProfile: 'ranged-control', skillConcepts: Object.freeze(['web-attack-speed-slow']) }),
     monster('blackstone-venom-hunter', '黑石毒獵手', 'normal', '使用淬毒弓箭的哥布林遠程怪', { faction: 'blackstone-goblins', race: 'goblin', attackProfile: 'ranged-poison', skillConcepts: Object.freeze(['poisoned-arrows']) }),
     monster('spider-nest-blackstone-beastmaster', '黑石訓獸師', 'elite', '操控並強化蜘蛛的毒液菁英', { faction: 'blackstone-bandits', attackProfile: 'beast-support-poison', skillConcepts: Object.freeze(['spider-command', 'venom-attack']) }),
--- tests/spider-nest-policy.test.js
+++ tests/spider-nest-policy.test.js
@@ -18,8 +18,10 @@ assert.equal(policy.getMonstersByRank('boss').length, 1);
 assert.equal(policy.getMonster('blackstone-venom-hunter').race, 'goblin');
 assert.equal(policy.getMonster('blackstone-venomblade-assassin').race, 'orc');
 assert.equal(policy.getMonster('giant-spider').bodyProfile, 'giant-bloated-abdomen');
-assert.ok(policy.MONSTERS.every((monster) => monster.level === 19 && monster.image === null && monster.stats === null
+assert.equal(policy.getMonster('venom-spitter-spider').image, 'assets/venom-spitter-spider.png');
+assert.ok(policy.MONSTERS.every((monster) => monster.level === 19 && monster.stats === null
   && monster.dropTableId === null && monster.aiProfileId === null && monster.skillIds.length === 0 && monster.implemented === false));
+assert.ok(policy.MONSTERS.filter((monster) => monster.id !== 'venom-spitter-spider').every((monster) => monster.image === null));
 assert.equal(policy.STORY.previousMapId, 'black-forest-trail');
 assert.equal(policy.STORY.nextMapId, 'blackstone-stronghold');
 assert.equal(policy.STORY.completionObjectiveId, 'defeat-giant-spider');
@@ -29,5 +31,8 @@ const path = require('node:path');
 const background = fs.readFileSync(path.join(__dirname, '..', policy.MAP.background));
 assert.equal(background.subarray(1, 4).toString(), 'PNG', 'the Spider Nest background is a PNG asset');
 assert.equal(background[25], 2, 'the Spider Nest background uses RGB color');
+const venomSpitter = fs.readFileSync(path.join(__dirname, '..', policy.getMonster('venom-spitter-spider').image));
+assert.equal(venomSpitter.subarray(1, 4).toString(), 'PNG', 'the venom spitter spider is a PNG asset');
+assert.equal(venomSpitter[25], 6, 'the venom spitter spider uses RGBA color with transparency');
 
 console.log('spider-nest-policy: assertions passed');
