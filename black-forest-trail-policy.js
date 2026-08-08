cc7a3c78192dd18614e61571f6c7e107b0dbb755
komay0818-coder <komay0818-coder@users.noreply.github.com>
2026-08-09 01:13:38 +0800

    feat: add transparent Blackstone poison spider artwork

--- black-forest-trail-policy.js
+++ black-forest-trail-policy.js
@@ -52,7 +52,7 @@
     monster('blackstone-trail-scout', '黑石斥候', 'normal', '巡邏與偵察'),
     monster('blackstone-trail-raider', '黑石掠奪者', 'normal', '近戰攔截與物資護送'),
     monster('blackstone-archer', '黑石弓箭手', 'normal', '遠程火力'),
-    monster('blackstone-poison-spider', '黑石毒蜘蛛', 'normal', '黑石圈養的毒系怪物', { faction: 'blackstone-beasts', ownerFaction: 'blackstone-bandits', tags: Object.freeze(['beast', 'poison', 'spider']) }),
+    monster('blackstone-poison-spider', '黑石毒蜘蛛', 'normal', '黑石圈養的毒系怪物', { image: 'assets/blackstone-poison-spider.png', faction: 'blackstone-beasts', ownerFaction: 'blackstone-bandits', tags: Object.freeze(['beast', 'poison', 'spider']) }),
     monster('blackstone-beastmaster', '黑石訓獸師', 'elite', '指揮與強化圈養蜘蛛'),
     monster('blackstone-captain', '黑石隊長', 'elite', '巡邏隊與補給線指揮官'),
     monster('blackstone-centurion', '黑石百夫長', 'boss', '守衛補給路線並持有蛛巢線索')
