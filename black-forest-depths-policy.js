92372e224e95eb922691428a63ffb367ad088aa1
komay0818-coder <komay0818-coder@users.noreply.github.com>
2026-08-10 04:35:42 +0800

    feat: add corrupted fallen druid image

--- black-forest-depths-policy.js
+++ black-forest-depths-policy.js
@@ -35,7 +35,7 @@
     monster('dark-spore-beast', '黑暗孢子獸', 'normal', 'purple-corruption'),
     monster('forest-spirit', '森林之魂', 'normal', 'green-nature'),
     monster('corrupted-blackstone-centurion', '腐化黑石百夫長', 'elite', 'purple-corruption'),
-    monster('corrupted-fallen-druid', '腐化墮落德魯伊', 'elite', 'purple-corruption'),
+    monster('corrupted-fallen-druid', '腐化墮落德魯伊', 'elite', 'purple-corruption', { image: 'assets/corrupted-fallen-druid.png' }),
     monster('heart-of-the-black-forest', '黑森林之心', 'boss', 'purple-corruption', { image: 'assets/heart-of-the-black-forest.png' })
   ]);
   const MONSTER_BY_ID = new Map(MONSTERS.map((entry) => [entry.id, entry]));
